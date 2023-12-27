6218fea7d05f79a4e108c003bb504f1828ee7c4e
Woomymy <woomy@woomy.be>
2023-12-27 16:04:08 +0100

    bin: Add lock script
    Change-Id: Idd03aaebcd266f2bdad61906dbf6f920434bfaee
    Signed-off-by: Woomymy <woomy@woomy.be>

---
+++ dotfiles/bin/power/lockscreen.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+FILE="$(mktemp -u)"
+TEXT="$(lsb_release -d | awk -F ":	" '{print $2}')"
+
+# Usage: apply_blur FILE
+apply_blur()
+{
+	# Temporary file used for conversion
+	TMP_FILE="$(mktemp -u)"	
+	SCREENSHOT="${1}"
+	
+	convert "${SCREENSHOT}" -filter Gaussian -blur 0x8 "${TMP_FILE}"
+	mv "${TMP_FILE}" "${SCREENSHOT}"
+}
+
+add_text()
+{
+	TMP_FILE="$(mktemp -u)"
+	SCREENSHOT="${1}"
+	convert -pointsize 72 -fill white -draw "text 50,1000 '${TEXT}'" "${SCREENSHOT}" "${TMP_FILE}"
+	mv "${TMP_FILE}" "${SCREENSHOT}"
+}
+
+main()
+{
+    WALLPAPER="$(ls /tmp/wallpaper-*.jpg)"
+    cp "${WALLPAPER}" "${FILE}"
+	apply_blur "${FILE}"
+	add_text "${FILE}"
+    
+    swaylock -i "${FILE}"
+    
+    rm "${FILE}"
+
+}
+
+main || {
+	notify-send -u "critical" "Failed to lock"
+}
