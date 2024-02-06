8ce8710e823a951d46899edd1dbb69ac7cb1d87c
Woomymy <woomy@woomy.be>
2024-02-06 13:32:05 +0100

    bin: Add new lock options
    * Super + Shift + L: Lock and turn off screens (screens are turned on
      after typing the password again)
    
    * Super + Alt + L: Lock, suspend, turn off screens and turn them back on
      after resume
    
    Change-Id: I1b68106df9173e1578ad323a36a357195335a65c
    Signed-off-by: Woomymy <woomy@woomy.be>

--- dotfiles/bin/power/lockscreen.sh
+++ dotfiles/bin/power/lockscreen.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -eo pipefail
 
 FILE="$(mktemp -u)"
 TEXT="$(lsb_release -d | awk -F ":	" '{print $2}')"
+
+# Turns on the screens
+ddc_poweron() {
+    for SCREEN in ${1}
+    do
+        ddcutil setvcp D6 1 --bus="${SCREEN}" || true
+    done
+}
+
+# Turns off the screens
+ddc_poweroff() {
+    for SCREEN in ${1}
+    do
+        ddcutil setvcp D6 4 --bus="${SCREEN}" || true
+    done
+}
 
 # Usage: apply_blur FILE
 apply_blur()
@@ -26,18 +42,32 @@ add_text()
 
 main()
 {
+    SCREENS="$(jq -r ".i2c_displays[]" "${HOME}/.bin/config.json")"
     WALLPAPER="$(ls /tmp/wallpaper-*.jpg)"
     cp "${WALLPAPER}" "${FILE}"
 	apply_blur "${FILE}"
 	add_text "${FILE}"
     
-    pulsemixer --toggle-mute
-    swaylock -i "${FILE}"
-    pulsemixer --toggle-mute
-    rm "${FILE}"
+    if [[ "${1}" == "--suspend" ]]
+    then
+        ddc_poweroff "${SCREENS}"
+        swaylock -i "${FILE}" &
+        systemctl suspend
+        # Systemctl suspend is asynchronous
+        # This is not the best solution and it probably needs to be fixed
+        sleep 2
+        ddc_poweron "${SCREENS}"
+    else
+        [[ "${1}" == "--screenoff" ]] && ddc_poweroff "${SCREENS}"
+        pulsemixer --toggle-mute
+        swaylock -i "${FILE}"
+        pulsemixer --toggle-mute
+        [[ "${1}" == "--screenoff" ]] && ddc_poweron "${SCREENS}"
+    fi
 
+    rm "${FILE}"
 }
 
-main || {
+main "${1}" || {
 	notify-send -u "critical" "Failed to lock"
 }
